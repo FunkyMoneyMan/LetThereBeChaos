26f54868327b7a63daaad8f7c3d7271b545c1f69
stink <StinkyDrizzel@gmail.com>
2021-08-26 20:49:54 -0400

    Added transitions and scenetransition controller

---
+++ Assets/Scripts/SceneTransition.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneTransition : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void NextScene()
+    {
+        GetComponent<Animator>().SetTrigger("Next");
+    }
+}
